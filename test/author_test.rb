69e27fc1d7682fb656cf26fe7b6167c2e308210e
David Atkins <atki1080@gmail.com>
2019-12-18 09:30:24 -0700

    Add author test suite

---
+++ test/author_test.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/author'
+require './lib/book'
+
+class AuthorTest < Minitest::Test
+  def setup
+    @charlotte_bronte = Author.new({
+      first_name: "Charlotte",
+      last_name: "Bronte"
+      })
+  end
+
+  def test_it_exists
+      assert_instance_of Author, @charlotte_bronte
+  end
+
+  def test_it_has_attributes
+    assert_equal "Charlotte Bronte", @charlotte_bronte.name
+    assert_equal [], @charlotte_bronte.books
+  end
+
+  def test_it_can_write_a_book
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    assert_equal Book, jane_eyre.class
+    assert_equal "Jane Eyre", jane_eyre.title
+    assert_equal "Charlotte Bronte", jane_eyre.author
+    assert_equal "1847", jane_eyre.publication_year
+  end
+end
