d8029517f78007aeeb8f3e77d20fe0990a39b83a
David Atkins <atki1080@gmail.com>
2019-12-18 10:55:19 -0700

    Add most_popular_book test case

--- test/library_test.rb
+++ test/library_test.rb
@@ -86,6 +86,7 @@ class LibraryTest < Minitest::Test
   def test_it_can_return_books
     @dpl.add_author(@charlotte_bronte)
     @dpl.add_author(@harper_lee)
+
     @dpl.checkout(@jane_eyre)
 
     assert_equal [@jane_eyre], @dpl.checked_out_books
@@ -100,5 +101,22 @@ class LibraryTest < Minitest::Test
     @dpl.checkout(@villette)
 
     assert_equal [@jane_eyre, @villette], @dpl.checked_out_books
+  end
+
+  def test_it_can_track_most_popular_book
+    @dpl.add_author(@charlotte_bronte)
+    @dpl.add_author(@harper_lee)
+
+    @dpl.checkout(@jane_eyre)
+    @dpl.return(@jane_eyre)
+    @dpl.checkout(@jane_eyre)
+    @dpl.checkout(@villette)
+    @dpl.checkout(@mockingbird)
+    @dpl.return(@mockingbird)
+    @dpl.checkout(@mockingbird)
+    @dpl.return(@mockingbird)
+    @dpl.checkout(@mockingbird)
+
+    assert_equal @mockingbird, @dpl.most_popular_book
   end
 end
