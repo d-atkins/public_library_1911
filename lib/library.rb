2a48ce1ddf1da268461a112d78d1ce17f3834ceb
David Atkins <atki1080@gmail.com>
2019-12-18 10:14:07 -0700

    Add library class

---
+++ lib/library.rb
@@ -0,0 +1,14 @@
+class Library
+  attr_reader :name, :books, :authors
+  def initialize(name)
+    @name = name
+    @books = []
+    @authors = []
+  end
+
+  def add_author(author)
+    @authors << author
+    @books << author.books
+    @books = @books.flatten
+  end
+end
